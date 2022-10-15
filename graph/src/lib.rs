835b37fd93ec076cbdeba0faad3d10c5ce9859f6
valadyan <valadyanich@mail.ru>
2022-10-15 22:40:59 +0300

    graph interface. deserialize

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -1,3 +1,69 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub struct Graph <T>{
+    nodes: Vec<GraphNode<T>>
+}
+
+impl<T> Graph<T> {
+    pub fn new(node_count: usize) -> Self { 
+        Self { nodes:  Vec::<GraphNode<T>>::with_capacity(node_count)} 
+    }
+
+    pub fn add_node(&mut self, data: T) -> usize {
+        self.nodes.push(GraphNode::<T>::new(data, Vec::new()));
+        self.nodes.len()
+    }
+
+    pub fn add_link(&mut self, from: usize, to: usize) {
+        self.nodes[from].links.push(to);
+    }
+
+    pub fn add_links(&mut self, from: usize, links: &mut Vec<usize>) {
+        self.nodes[from].links.append(links);
+    }
+
+    pub fn serialize(&mut self, filename: &str){
+        let path = Path::new(filename);
+        let display = path.display();
+    
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        file.write(&self.nodes.len().to_le_bytes());
+        file.write(b"\n");
+
+        for node in &self.nodes{
+            file.write(node.data.to_le_bytes());
+            file.write(b"\n");
+
+            for link in node.links{
+                file.write(link.to_le_bytes());
+                file.write(b" ");
+            }
+
+            file.write(b"\n");
+        }
+
+        match file.write_all(LOREM_IPSUM.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+    }
+}
+
+struct GraphNode<T>{
+    data: T,
+    links: Vec<usize>
+}
+
+impl<T> GraphNode<T> {
+    fn new(data: T, links: Vec<usize>) -> Self { Self { data, links } }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate graph;
+
+use graph::Graph;
+
+use std::collections::btree_map::Iter;
+use std::fs::File;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(_graph) = deserialize_graph("testGraph"){
+        _graph.nodes;//serialize("asdf");
+    }
+}
+
+fn deserialize_graph(filename: &str) -> io::Result<Graph<&str>>{
+    let file = File::open(filename)?;
+    let mut reader: io::BufReader<File> = io::BufReader::new(file);
+
+    let mut _graph: Graph<&str> = init_graph(&mut reader);
+    fill_graph(&mut _graph, &mut reader)
+
+    Ok(_graph)
+}
+
+fn init_graph(reader: &mut io::BufReader<File>)-> Graph<&'static str>{
+    let mut nodes_count =String::new();
+    reader.read_line(&mut nodes_count);
+    nodes_count = nodes_count.trim().to_string();
+
+    Graph::new(nodes_count.parse::<usize>().unwrap())
+}
+
+fn fill_graph(graph: &mut Graph<&str>, reader: &mut io::BufReader<File>){
+    for line in reader.lines() {
+        if let Ok(node_str) = line {
+            // print!("{}", node_str);
+            let mut _nodeInf: Vec<&str> = node_str.split(" ").collect();
+            let data = _nodeInf[0];
+            let node_id = graph.add_node(data);
+
+            for n in 1.._nodeInf.len() {
+                graph.add_link(node_id, _nodeInf[n].parse::<usize>().unwrap());
+            }
+        }
+    }
 }
