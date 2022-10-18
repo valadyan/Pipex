5b803bb0b8c4a792775b7231db736852ff0cb9e4
valadyan <valadyanich@mail.ru>
2022-10-18 21:35:14 +0300

    serialize

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -2,7 +2,8 @@ use std::collections::HashMap;
 use std::fs::{File, self};
 use std::io::{self, BufRead, Write};
 use std::path::Path;
-use std::fmt::Display;
+use std::fmt::{Display};
+use std::error::Error;
 
 pub struct Graph <T>{
     nodes: HashMap<String, GraphNode<T>>
@@ -17,24 +18,22 @@ impl<T> GraphNode<T> {
     fn new(data: T) -> Self { Self { data, links: Vec::new() } }
 }
 
-impl<T> Graph<T> {
-    pub fn new(node_count: usize) -> Self { 
+impl<T: Display> Graph<T> {
+    pub fn new() -> Self { 
         Self { nodes : HashMap::new() } 
     }
 
-    pub fn add_node(&mut self, node_id: String, node_data: T){
+    pub fn add_node(&mut self, node_id: &str, node_data: T){
         self.nodes.insert(node_id.to_string(), GraphNode::new(node_data));
     }
 
-    pub fn add_link(&mut self, from: String, to: String) {
+    pub fn add_link(&mut self, from: &str, to: &str) {
         match self.nodes.get_mut(&from.to_string()) {
             None =>{},
             Some(node) => node.links.push(to.to_string())
         }
     }
-}
 
-impl<T: Display> Graph<T> {
     pub fn serialize(&self, filename: &str){
         let path = Path::new(filename);
         let display = path.display();
@@ -60,42 +59,49 @@ impl<T: Display> Graph<T> {
 
     pub fn deserialize(filename: &str) -> io::Result<Graph<String>>{
         let file = fs::File::open(filename)?;
-        let mut reader: io::BufReader<File> = io::BufReader::new(file);
+        let reader: io::BufReader<File> = io::BufReader::new(file);
     
-        let mut _graph: Graph<String> = Self::init_graph(&mut reader);
-        Self::fill_nodes(&mut _graph, &mut reader);
-    
-        Ok(_graph)
-    }
-    
-    fn init_graph(reader: &mut io::BufReader<File>)-> Graph<String>{
-        let mut nodes_count =String::new();
-        reader.read_line(&mut nodes_count);
-        let t = nodes_count.trim();
-    
-        Graph::new(nodes_count.parse::<usize>().unwrap())
-    }
-    
-    fn fill_nodes(graph: &mut Graph<String>, reader: &mut io::BufReader<File>){
-        // let mut buf = vec![];
-        // reader.read_until(b" ", &mut buf);
-        // graph.add_node(buf);
+        let mut graph = Graph::<String>::new();
+        let mut read_links = false;
 
-        // for line in reader.lines() {
-        //     if let Ok(node_str) = line {
-        //         let mut _nodeInf: Vec<&str> = node_str.split(" ").collect();
-        //         let data = _nodeInf[0].to_string();
-        //         let node_id = graph.add_node(data);
-                
-        //         if _nodeInf.len() < 2 {continue;}
+        for line_res in reader.lines() {
+            match line_res {
+                Err(mes) => return Err(mes),
+                Ok(line) => {
+                    Self::is_reader_read_links(&mut read_links, &line);
 
-        //         let links = &_nodeInf[1..].to_vec();
-        //         for n in 1.._nodeInf.len() {
-        //             graph.add_link(node_id, _nodeInf[n].parse::<usize>().unwrap());
-        //         }
-        //     }
-        // }
+                    if read_links {
+                        Self::parse_links(&mut graph, &line);
+                    } else {
+                        Self::parse_nodes(&mut graph, &line);
+                    }
+                }
+            }
+        }
+
+        Ok(graph)
     }
 
+    fn is_reader_read_links(read_links: &mut bool, file_line: &str){
+        if !*read_links && file_line.starts_with("#") {
+            *read_links = true;
+        }
+    }
 
+    fn parse_nodes(graph: &mut Graph<String>, node_info: &str){
+        let node_id_bound = node_info.find(char::is_whitespace).expect("incorrect graph file");
+        let node_id = &node_info[..node_id_bound];
+        let mut node_data = "";
+        if node_id_bound < node_info.len() {
+            node_data = &node_info[node_id_bound+1..];
+        } 
+        graph.add_node(node_id, node_data.to_owned());
+    }
+    
+    fn parse_links(graph: &mut Graph<String>, link_info: &str){
+        let from_to: Vec<&str> = link_info.split_whitespace().collect();
+        if from_to.len() == 2 {
+            graph.add_link(from_to[0], from_to[1]);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,7 @@ extern crate graph;
 use graph::Graph;
 
 fn main() {
-    let mut gr = Graph::<&str>::new(3);
-    gr.add_node(1.to_string(), "asc");
-    gr.add_node(2.to_string(), "cc");
-    gr.add_node(3.to_string(), "");
-    gr.add_link(1.to_string(),2.to_string());
-    gr.add_link(3.to_string(),2.to_string());
-
-    gr.serialize("testgr")
-    // if let Ok(_graph) = Graph::<String>::deserialize("testGraph") {
-    //     _graph.serialize("asdf");
-    // }
+    if let Ok(_graph) = Graph::<String>::deserialize("testGraph") {
+        _graph.serialize("asdf");
+    }
 }
